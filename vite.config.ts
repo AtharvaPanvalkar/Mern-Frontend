70beefd0dbc26a94bf3194a667be958c1be9aadc
AtharvaPanvalkar <atharvaextra11@gmail.com>
2025-04-19 00:16:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  
   optimizeDeps: {
-    include: ["jwt-decode"], // Force Vite to pre-bundle `jwt-decode` if it's causing issues
+    include: ["jwt-decode,"], // Force Vite to pre-bundle `jwt-decode` if it's causing issues
   },
+    build: {
+    rollupOptions: {
+      external: ['qrcode.react'],
+    },}
+  
 });
